e60dc63f0f9f4285d476719c0101699ebe79151e
An Do Van (Intern-Tech Pools-Technology-VND HO) <an.do@vndirect.com.vn>
2021-06-02 23:11:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import 'slick-carousel/slick/slick.scss';
 import './App.scss';
 import FooterInfo from './component/footer/FooterInfo';
 import FooterTag from './component/footer/FooterTag';
+import Footer_bottom from './component/footer/Footer_bottom';
 import MailSign from './component/mailSign/MailSign';
 import MapStore from './component/mapStore/MapStore';
 import MenuBar from './component/menu/MenuBar';
 import MenuMobile from './component/menu/MenuMobile';
+import ButtonScroll from './component/scrollTop/buttonScroll';
 import SideBar from './component/sideBar/SideBar';
 import Home from './pages/home';
 import { showMenu } from './store/menuMobile';
@@ -31,7 +33,9 @@ function App() {
         <FooterTag />
         <MailSign />
         <FooterInfo />
+        <Footer_bottom />
       </div>
+      <ButtonScroll />
 
       {data ? <div className='close_menu' onClick={handlerCloseMenu}></div> : null}
     </div>
---
+++ src/component/scrollTop/ButtonScroll.js
@@ -0,0 +1,13 @@
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+function ButtonScroll(props) {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faChevronUp} />
+    </div>
+  );
+}
+
+export default ButtonScroll;
